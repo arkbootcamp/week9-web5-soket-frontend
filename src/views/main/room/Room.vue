d684be57aff39c1a85f906e4257f1420b4497e3a
risano <muhammadrisano@gmail.com>
2020-12-23 17:22:30 +0700

    add format message

--- src/views/main/room/Room.vue
+++ src/views/main/room/Room.vue
@@ -6,7 +6,7 @@
       <div class="col-8">
       <ul class="list-group">
   <li class="list-group-item active" >Nama ROOM :  {{$route.query.room}}</li>
-  <li class="list-group-item" v-for="(message, index ) in messages" :key="index">{{message}}</li>
+  <li class="list-group-item" v-for="(message, index ) in messages" :key="index">{{message.senderId}} => {{message.message}} || {{message.time}}</li>
 </ul>
     <div class="input-group mb-3">
   <input type="text" class="form-control" v-model="inputMessage" @keypress.enter="handleClick" placeholder="input message">
@@ -28,12 +28,15 @@ export default {
     return {
       messages: [],
       inputMessage: '',
-      room: ''
+      room: '',
+      username: ''
     }
   },
   methods: {
     handleClick () {
-      this.socket.emit('reciverMessage', { room: this.room, message: this.inputMessage })
+      this.socket.emit('reciverMessage', { room: this.room, message: this.inputMessage, senderId: this.username }, (message) => {
+        this.messages.push(message)
+      })
       this.inputMessage = ''
     }
   },
@@ -41,6 +44,7 @@ export default {
     const username = this.$route.query.username
     const room = this.$route.query.room
     this.room = room
+    this.username = username
     this.socket.emit('initialUser', { username: username, room: room })
     this.socket.on('kirimKembali', (data) => {
       this.messages.push(data)
